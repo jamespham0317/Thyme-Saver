4d132d5a47c513d7c8232e5170f8a757b83fe907
jamespham <jpham07@student.ubc.ca>
2025-03-10 18:45:49 -0700

    Test http

--- recipe-finder/src/components/Register/Register.js
+++ recipe-finder/src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://recipe-finder-production-d134.up.railway.app/register', {
+    fetch('http://recipe-finder-production-d134.up.railway.app/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
