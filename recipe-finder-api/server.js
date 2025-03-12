8ce1ee6d04138fb1829d68a84e9a5bf12c661523
jamespham <jpham07@student.ubc.ca>
2025-03-11 19:58:54 -0700

    Working backend

--- recipe-finder-api/server.js
+++ recipe-finder-api/server.js
@@ -52,11 +52,6 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
-
-app.get('/test', (req, res) => {
-  res.status(200).json({ message: 'Backend is working!' });
-});
-
 app.listen(process.env.PORT, ()=> {
   console.log('app is running on port ${process.env.PGPORT}');
 })
